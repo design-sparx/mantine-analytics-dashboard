9c28f0c9a51837d94db94bdaff58720ccc76043d
kelvin <kelvin.kiprop96@gmail.com>
2025-06-25 13:48:30 +0300

    fix: fixed image assets loading by reworking middleware.ts

---
+++ constants/tech-stack.ts
@@ -0,0 +1,36 @@
+export const TECH_STACK = [
+  { title: 'nextjs', version: '14.2.26', href: 'https://nextjs.org/' },
+  { title: 'react', version: '18.2.0', href: 'https://react.dev/' },
+  {
+    title: 'typescript',
+    version: '5.1.6',
+    href: 'https://www.typescriptlang.org/',
+  },
+  { title: 'mantine', version: '7.14.3', href: 'https://mantine.dev/' },
+  {
+    title: 'tabler icons',
+    version: '3.33.0',
+    href: 'https://tabler-icons.io/',
+  },
+  { title: 'tiptap', version: '2.10.3', href: 'https://tiptap.dev/' },
+  { title: 'apexcharts', version: '3.44.0', href: 'https://apexcharts.com/' },
+  { title: 'dayjs', version: '1.11.13', href: 'https://day.js.org/' },
+  { title: 'fullcalendar', version: '6.1.8', href: 'https://fullcalendar.io/' },
+  { title: 'dnd-kit', version: '6.0.8', href: 'https://dndkit.com/' },
+  {
+    title: 'embla-carousel',
+    version: '8.0.0',
+    href: 'https://www.embla-carousel.com/',
+  },
+  {
+    title: 'mantine datatable',
+    version: '7.1.7',
+    href: 'https://icflorescu.github.io/mantine-datatable',
+  },
+  { title: 'lodash', version: '4.17.21', href: 'https://lodash.com/' },
+  {
+    title: 'react simple maps',
+    version: '3.0.0',
+    href: 'https://www.react-simple-maps.io/',
+  },
+];
